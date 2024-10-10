ee726adba96729032d11f9ad506dc9a0b4e7dcc2
injili <nyarekigospel@gmail.com>
2024-10-10 10:56:31 +0300

    pause for a figma prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-
+import { useState } from "react";
+import VideoRoom from "./components/VideoRoom";
 
 export default function App() {
+  const [joined, setJoined] = useState(false);
+
   return (
-    <div className="bg-red-500 h-screen w-screen">This App</div>
+    <div className="bg-red-500 h-screen w-full">
+      <h1 className="font-bold text-7xl p-8">SnuggleMeet</h1>
+
+      {
+        !joined && (
+          <button onClick={() => setJoined(true)}>
+            Join Room
+          </button>
+        )
+      }
+
+      {
+        joined && (
+          <VideoRoom/>
+        )
+      }
+    </div>
   )
 }
---
+++ src/components/VideoRoom.jsx
@@ -0,0 +1,7 @@
+export default function VideoRoom() {
+    return (
+        <div>
+            <p>VideoRoom</p>
+        </div>
+    )
+}
