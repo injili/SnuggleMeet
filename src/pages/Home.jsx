04ea7153357b5fcc7c162a4bb1f9acb6623ee5e3
injili <nyarekigospel@gmail.com>
2024-11-11 23:07:43 +0300

    there's progress to be made

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,44 @@
 import theLogo from '../assets/img/snuggle.png';
+import ReactDOM from 'react-dom/client';
 
 export default function Home() {
 
-    const openLoginPage = () => {
-        chrome.tabs.create({
-            url: "https://dev-wddm4b32vawbrco2.us.auth0.com/u/login?state=hKFo2SBnRlFnNzIySlVKZXdxT1luMHhtMF96N0hpWW9xbnR6N6Fur3VuaXZlcnNhbC1sb2dpbqN0aWTZIDdjRXd6WjVRd3pIRXZUR2pPMDdkY3lSQXIwQ2p5SG5mo2NpZNkgbUJiV1BrOE93czd3NUxOMGFLZnlLWXdsdkhPTGdIb0w"
-        })
+    const openWindow = async () => {
+        try {
+            const dpip = await window.documentPictureInPicture.requestWindow({
+                width: "1000",
+                height: "1000",
+            });
+
+            const pipDiv = dpip.document.createElement("div");
+            pipDiv.setAttribute("id", "pip-root");
+            dpip.document.body.append(pipDiv);
+            const pipRoot = ReactDOM.createRoot(
+                dpip.document.getElementById("pip-root")
+            );
+            pipRoot.render(<WindowContents />);
+        } catch (error) {
+            if (error) {
+                console.log(error);
+            }
+        }
     }
+
+    const WindowContents = () => {
+        return (
+            <div>
+                <p>This Window is open</p>
+            </div>
+        );
+    };
+
     return (
         <div  className="h-[600px] w-[600px] flex flex-col gap-12 justify-center items-center">
             
             <img src={theLogo} alt="the snugglemeet logo" className="w-[230px] h-[230px]"/>
             <h1 className="font-bowldy text-6xl">welcome</h1>
-            <button className='text-lg font-alata font-semibold py-2 px-12 border border-2 border-second bg-third' onClick={openLoginPage}>Log In</button>
-            <p className='text-base font-semibold font-alata'>Don't Have an account? <button className='text-forth hover:underline hover:underline-offset-4 font-medium hover:font-bold'>Sign Up</button></p>
+            <button className='text-lg font-alata font-semibold py-2 px-12 border border-2 border-second bg-third' onClick={openWindow}>Log In</button>
+            <p className='text-base font-semibold font-alata'>Don&apos;t Have an account? <button className='text-forth hover:underline hover:underline-offset-4 font-medium hover:font-bold'>Sign Up</button></p>
 
 
         </div>
