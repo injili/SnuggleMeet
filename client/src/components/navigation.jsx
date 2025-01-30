df4030051bb18ddaf90b4857c5a57ca9fd95f9bd
injili <nyarekigospel@gmail.com>
2025-01-31 01:05:06 +0300

    Home page updated

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -1,14 +1,24 @@
 import { useLocation } from "react-router-dom";
 import theLogo from "../assets/img/icon.png";
+import { useAuth } from "../api/context";
 
 export default function Navigation() {
+  const { currentUser } = useAuth();
   const location = useLocation();
 
   const checkLocation = () => {
     if (location.pathname === "/") {
       return <h1 className="text-2xl font-alata ">Sign In Page</h1>;
     } else if (location.pathname === "/signup") {
-      return <h1 className="text-2xl font-alata ">Sign Up Page</h1>;
+      return <h1 className="text-3xl font-alata ">Sign Up Page</h1>;
+    } else if (location.pathname === "/home") {
+      return (
+        <h1 className="text-3xl font-alata ">
+          {currentUser.displayName
+            ? currentUser.displayName
+            : currentUser.email}
+        </h1>
+      );
     }
   };
   return (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,64 +1,116 @@
-import { Link, useNavigate, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../api/context";
-import { doSignOut } from "../api/auth";
-import theLogo from "../assets/img/icon.png";
 export default function Home() {
-  const navigate = useNavigate();
   const { currentUser } = useAuth();
+  const navigate = useNavigate();
   return (
-    <div className="min-h-screen">
+    <div className="">
       {!currentUser && <Navigate to={"/"} replace={true} />}
       {!currentUser.emailVerified && (
         <Navigate to={"/verification"} replace={true} />
       )}
-      <div className="h-full p-8 flex flex-col gap-8 justify-center items-center">
-        <div className="w-full flex justify-between items-center">
-          <div className="flex gap-4 items-center">
-            <img
-              src={theLogo}
-              alt="our logo snugglemeet"
-              height={39}
-              width={39}
-            />
-            <Link to="/profile">
-              <h1 className="font-bolanosima font-semibold text-second tracking-wide text-2xl">
-                {currentUser.displayName
-                  ? currentUser.displayName
-                  : currentUser.email}
-              </h1>
-            </Link>
-          </div>
-          <div>
-            <button
-              onClick={() => {
-                doSignOut().then(() => {
-                  navigate("/");
-                });
-              }}
-              className="py-1 text-second font-semibold font-alata px-4 border border-2 bg-third border-second"
-            >
-              LOG OUT
-            </button>
-          </div>
-        </div>
+      <div className="h-full p-4 flex flex-col gap-4 justify-center ">
         <p className="font-alata">
           Welcome to SnuggleMeet, what room would you like to join?
         </p>
-        <div className="flex items-center w-full gap-12 justify-between">
-          <Link
-            to="/room"
-            className="w-full font-bolanosima font-semibold text-4xl text-second aspect-square border-2 border-second bg-third p-2 flex justify-center"
-          >
-            <button>ROOM A</button>
-          </Link>
 
-          <button className="w-full font-bolanosima font-semibold text-4xl text-second aspect-square border-2 border-second bg-third p-2">
-            ROOM B
-          </button>
+        <div className="flex items-center w-full gap-4 justify-between">
+          <div className="w-full rounded-[15px] bg-second">
+            <div className="flex justify-between items-center m-4 pb-4 border-b border-first">
+              <div className="px-2 py-1 font-montserrat text-xs font-medium rounded-full bg-first">
+                12 participants
+              </div>
+              <p className="text-xl text-first font-alata">Room A</p>
+            </div>
+            <div className="flex justify-between items-center m-4">
+              <p className="text-xs py-1 text-first font-medium font-montserrat">
+                injili, tweet, scale, trees
+              </p>
+              <button
+                onClick={() => {
+                  navigate("/room");
+                }}
+                className="py-1 bg-first rounded-full text-third text-xs font-montserrat font-medium hover:bg-third px-8 hover:text-first "
+              >
+                Join
+              </button>
+            </div>
+          </div>
+          <div className="w-full rounded-[15px] bg-third">
+            <div className="flex justify-between items-center m-4 pb-4 border-b border-first">
+              <div className="px-2 py-1 font-montserrat text-xs font-medium rounded-full bg-first">
+                Full
+              </div>
+              <p className="text-xl text-first font-alata">Room B</p>
+            </div>
+            <div className="flex justify-between items-center m-4">
+              <p className="text-xs py-1 text-first font-medium font-montserrat">
+                tress, tando, dimple, sweep, ...
+              </p>
+            </div>
+          </div>
         </div>
-        <p className="font-alata">
-          Powered by <span className="text-forth text-xl">injili.tech</span>
-        </p>
+        <div className="flex items-center w-full gap-4 justify-between">
+          <div className="w-full rounded-[15px] bg-second">
+            <div className="flex justify-between items-center m-4 pb-4 border-b border-first">
+              <div className="px-2 py-1 font-montserrat text-xs font-medium rounded-full bg-first">
+                4 participants
+              </div>
+              <p className="text-xl text-first font-alata">Kastomaa</p>
+            </div>
+            <div className="flex justify-between items-center m-4">
+              <p className="text-xs py-1 text-first font-medium font-montserrat">
+                injili, tweet, scale, trees
+              </p>
+              <button
+                onClick={() => {
+                  navigate("/room");
+                }}
+                className="py-1 bg-first rounded-full text-third text-xs font-montserrat font-medium hover:bg-third px-8 hover:text-first"
+              >
+                Join
+              </button>
+            </div>
+          </div>
+          <div className="w-full rounded-[15px] bg-second">
+            <div className="flex justify-between items-center m-4 pb-4 border-b border-first">
+              <div className="px-2 py-1 font-montserrat text-xs font-medium rounded-full bg-first">
+                10 participants
+              </div>
+              <p className="text-xl text-first font-alata">Kristals</p>
+            </div>
+            <div className="flex justify-between items-center m-4">
+              <p className="text-xs py-1 text-first font-medium font-montserrat">
+                injili, tweet, scale, trees
+              </p>
+              <button
+                onClick={() => {
+                  navigate("/room");
+                }}
+                className="py-1 bg-first rounded-full text-third text-xs font-montserrat font-medium hover:bg-third px-8 hover:text-first"
+              >
+                Join
+              </button>
+            </div>
+          </div>
+        </div>
+        <button
+          onClick={() => {
+            navigate("/room");
+          }}
+          className="py-1 w-2/6 flex items-center justify-center gap-2 bg-first rounded-full text-third text-sm font-montserrat font-semibold hover:bg-third hover:text-first border border-2 border-third"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="8"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" />
+          </svg>
+          Create Room
+        </button>
       </div>
     </div>
   );
