a5e787a36de8f1acac141c6eba917d287d124564
injili <nyarekigospel@gmail.com>
2025-02-06 14:44:13 +0300

    here we go

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
       )}
       <div className="h-full p-4 flex flex-col gap-4 justify-center ">
         <p className="font-alata">
-          Welcome to SnuggleMeet, what room would you like to join?
+          Welcome to Valediktoria, what room would you like to join?
         </p>
 
         <div className="flex items-center w-full gap-4 justify-between">
